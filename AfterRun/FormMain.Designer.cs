ab2da0c9fe2027e352e87c93ec1885fb0837cd28
svnuser <svnuser@5ada3c48-002d-0410-850d-99865cc6fd0f>
2009-03-04 02:36:51 +0000

    start
    git-svn-id: svn://192.168.3.99/AfterRun@1433 5ada3c48-002d-0410-850d-99865cc6fd0f

---
+++ AfterRun/FormMain.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace AfterRun
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必要なデザイナ変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナで生成されたコード
+
+        /// <summary>
+        /// デザイナ サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディタで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.timerMain = new System.Windows.Forms.Timer(this.components);
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(109, 50);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(83, 21);
+            this.btnOK.TabIndex = 0;
+            this.btnOK.Text = "&OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(198, 49);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(82, 22);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "&Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // timerMain
+            // 
+            this.timerMain.Enabled = true;
+            this.timerMain.Interval = 1000;
+            this.timerMain.Tag = "10";
+            this.timerMain.Tick += new System.EventHandler(this.timerMain_Tick);
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Location = new System.Drawing.Point(11, 11);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(0, 12);
+            this.labelTitle.TabIndex = 2;
+            // 
+            // FormMain
+            // 
+            this.AcceptButton = this.btnOK;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(292, 83);
+            this.Controls.Add(this.labelTitle);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormMain";
+            this.Text = "AfterRun";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Timer timerMain;
+        private System.Windows.Forms.Label labelTitle;
+
+
+    }
+}
+
---
+++ AfterRun/FormMain.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using CommandLine.Utility;
+
+namespace AfterRun
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Launch()
+        {
+            String[] args = Environment.GetCommandLineArgs();
+            for (int i = 1; i < args.Length; ++i)
+            {
+                System.Diagnostics.Process.Start(args[i]);
+            }
+            Close();
+        }
+
+        private void timerMain_Tick(object sender, EventArgs e)
+        {
+            int n = Int32.Parse(timerMain.Tag.ToString());
+            n--;
+            if (n < 0)
+            {
+                Launch();
+            }
+
+            btnOK.Text = "OK" + " (" + n + ")";
+            timerMain.Tag = n;
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            String[] args = Environment.GetCommandLineArgs();
+
+            if (args.Length < 2)
+            {
+                MessageBox.Show("ˆø”‚ª‚ ‚è‚Ü‚¹‚ñB");
+                Close();
+            }
+
+            labelTitle.Text = args[1] + " ‚ð‹N“®‚µ‚Ü‚·B";
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            Launch();
+        }
+    }
+}
